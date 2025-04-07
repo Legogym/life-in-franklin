859822e03d80f79d68fdada3e4226c94d92f04d7
Tyler Kimberlin <tylermkimberlin@gmail.com>
2025-04-07 18:47:03 -0400

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/life-in-franklin/", // Ensures correct path resolution on GitHub Pages
-});
+  base: '/', // <--- Set this to '/' for Vercel
+})
